d7a9f05431dfd719dfe46e4f9f992d8fc351fd2b
rkukulski <39187880+rkukulski@users.noreply.github.com>
2024-08-19 15:17:36 +0200

    Update fidelity_optimal_qubit_causal.jl

--- fidelity_optimal_qubit_causal.jl
+++ fidelity_optimal_qubit_causal.jl
@@ -7,14 +7,21 @@ using SparseArrays
 function projector_s_s(N=1, atol=1e-3)
     
     """
-        Function creating the projector onto commutator subspace.
-        Usage: Take matrix X. Define |Y>> := projector_s_s(N) |X>>.
-        Then, [Y,U^{⊗N+1}] = 0 for all U.
+        Input: N - the number of copies of the given measurement
+               atol - tolerance level used to numerically create 
+               a projection matrix with tested default vaule 10^(-3)
+
+        Output: The projector onto commutator subspace.
+
+        Note: Take matrix X. Define |Y>> := projector_s_s(N) |X>>.
+               Then, [Y,U^{⊗N+1}] = 0 for all qubit unitary U.
     """
 
+    # P is the projector created on 2 x (N+1) = 2N+2 subsystems
     P = zeros(2^(2N+2), 2^(2N+2))
     idx = CartesianIndices(Tuple(fill(2, N+1)))
 
+    # P |ω_π>> = |ω_π>> for any system permutation matrix ω_π defined on N+1 systems
     for perm in permutations(1:N+1)
         x = zeros(fill(2, 2N+2)...)
         for k1 in idx
@@ -25,6 +32,8 @@ function projector_s_s(N=1, atol=1e-3)
         P += x * x'
     end
 
+    # P is positive semidefinite matrix defined on the space spanned by |ω_π>>. 
+    # It has to be normalized (eigenvalues 0 and 1) 
     F = eigen(P)
     Λ = Diagonal([x > atol ? 1 : 0 for x in F.values])
     P0 = F.vectors * Λ * F.vectors'
@@ -34,7 +43,10 @@ end
 function Qpartialtrace(Q, tuple)
 
     """
-        Function defining partial trace of Q over qubit spaces X_j such that tuple[j] = 0.
+        Input: Q - a matrix of size 2^N x 2^N, where N is some integer
+               tuple - binary tuple of size N
+
+        Output: Partial trace of Q over qubit spaces X_j such that tuple[j] = 0.
     """
     
     N = Int(log2(size(Q, 1)))
@@ -51,41 +63,66 @@ end
 function calc_causal(N)
 
     """
-        Function calculating the maximal value of the fidelity function for 
-        d = 2 and N copies of given von Neumann measurement.  
+        Input: N - the number of copies of the given measurement
+
+        Output: The maximal value of the fidelity function F_d(N) for d = 2
+                and N copies of given von Neumann measurement.  
+
+        Note: We use SDP program defined in the paper as Program II. Additionally, we
+              use two simplifications explained in Section IV for the case d = 2.
     """
 
+    # P is the projector such that P |ω_π>> = |ω_π>> for any 
+    # system permutation matrix ω_π defined on N+1 qubit systems
     P = projector_s_s(N)
 
-    Rs = [ComplexVariable(2^(N+1), 2^(N+1)) for _=1:2^(N+1)]
-    constraints = [R in :SDP for R in Rs]
-    constraints += [P * vec(R) == vec(R) for R in Rs]
+    # Lijs is a list of 2^(N+1) operators L_{i,j} that operate on the 
+    # space of size 2^(N+1)
+    Lijs = [ComplexVariable(2^(N+1), 2^(N+1)) for _=1:2^(N+1)]
 
-    Rs = reshape(Rs, fill(2, N+1)...)
+    # Lij ≥ 0
+    constraints = [Lij in :SDP for Lij in Lijs]
 
-    S = [sum(Rs[j, i] for i=1:2) for j ∈ CartesianIndices(Tuple(fill(2, N)))]
-    S = reshape(S, fill(2, N)...)
-    constraints += [S[j] == partialtrace(S[j], N+1, fill(2, N+1)) ⊗ I(2)/2 for j=1:2^N]
-    S = [partialtrace(X, N+1, fill(2, N+1))/2 for X ∈ S]
-    S = reshape(S, fill(2, N)...)
-    trW = sum(tr(X) for X in S)
+    # [Lij, \bar(U) ⊗ U^(⊗N)] = 0, were we equivalently assume
+    # [Lij, U^(⊗N+1)] = 0 by using projector P
+    constraints += [P * vec(Lij) == vec(Lij) for Lij in Lijs]
+
+    Lijs = reshape(Lijs, fill(2, N+1)...)
+
+    # Partial sum of Lijs
+    SumLijs = [sum(Lijs[j, i] for i=1:2) for j ∈ CartesianIndices(Tuple(fill(2, N)))]
+    SumLijs = reshape(SumLijs, fill(2, N)...)
+
+    # Network constraint
+    constraints += [SumLijs[j] == partialtrace(SumLijs[j], N+1, fill(2, N+1)) ⊗ I(2)/2 for j=1:2^N]
+
+
+    SumLijs = [partialtrace(SumLij, N+1, fill(2, N+1))/2 for SumLij ∈ SumLijs]
+    SumLijs = reshape(SumLijs, fill(2, N)...)
+
+    # Trace constraint
+    trW = sum(tr(SumLij) for SumLij ∈ SumLijs)
     constraints += [real(trW) == 2^N]
     
+    # Process matrix constraint
     IDX = CartesianIndices(Tuple(fill(0:1, N)))[2:2^N]
-    for i in IDX
-        Si = [X for X in S]
-        Si = [Qpartialtrace(X, i) for X in Si]
-        Si = reshape(Si, fill(2, N)...)
-        Hi = sum((-1)^(dot(Tuple(i), Tuple(j))) * Si[j] for j in CartesianIndices(Tuple(fill(2, N))))
-        constraints += [Hi == zeros(2^sum(Tuple(i)), 2^sum(Tuple(i)))]
+    for k in IDX
+        Sk = [SumLij for SumLij in SumLijs]
+        Sk = [Qpartialtrace(X, k) for X in Sk]
+        Sk = reshape(Sk, fill(2, N)...)
+        Hk = sum((-1)^(dot(Tuple(k), Tuple(j))) * Sk[j] for j in CartesianIndices(Tuple(fill(2, N))))
+        constraints += [Hk == zeros(2^sum(Tuple(k)), 2^sum(Tuple(k)))]
     end
 
-    linear = LinearIndices(Rs)
+    # objective function
+    linear = LinearIndices(Lijs)
     t = 1/2 * real(
         sum(
-            Rs[CartesianIndex(reverse(Tuple(a)))][linear[a], linear[a]] for a in CartesianIndices(Rs)
+            Lijs[CartesianIndex(reverse(Tuple(a)))][linear[a], linear[a]] for a in CartesianIndices(Lijs)
         )
         )
+
+    # SDP optimization
     problem = maximize(t, constraints)
     solve!(problem, Convex.MOI.OptimizerWithAttributes(SCS.Optimizer, "eps_abs" => 1e-8, "eps_rel" => 1e-8))
 
